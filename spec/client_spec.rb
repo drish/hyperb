faca5a17ce50c5c4f33dbedc4e78ee47c88c93c7
drish <d.erich@hotmail.com>
2017-07-28 21:14:44 -0400

    block test

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,8 +2,17 @@ require "helper"
 
 RSpec.describe Hyperb::Client do
 
+  it 'simple init' do
+    client = Hyperb::Client.new do |c|
+      c.access_key = 'k'
+      c.secret_key = 's'
+    end
+    expect(client.access_key).to eq 'k'
+    expect(client.secret_key).to eq 's'
+  end
+
   describe '#credentials?' do
-    
+
     it 'returns true if all credentials are set' do
       client = Hyperb::Client.new(secret_key: 'SK', access_key: 'AK')
       expect(client.credentials?).to be true
