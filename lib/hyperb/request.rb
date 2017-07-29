86ee962f26e5d7914e0cb69a2581a113256424c7
drish <d.erich@hotmail.com>
2017-07-29 19:05:20 -0400

    impl hyper authentication scheme

--- lib/hyperb/request.rb
+++ lib/hyperb/request.rb
@@ -1,61 +1,77 @@
 require 'openssl'
 require 'time'
+require 'uri'
+require 'http'
+require 'digest'
 
 module Hyperb
 
   class Request
 
-    FMT = '%Y%m%dT%H%M%SZ'
-    VERSION = 'v1.23'
-    HOST = 'us-west-1.hyper.sh'
-    REGION = 'us-west-1'
-    SERVICE = 'hyper'
-    ALGORITHM = 'HYPER-HMAC-SHA256'
-    KEYPARTS_REQUEST = 'hyper_request'
-    BASE_URL = 'https://' + HOST + '/' + VERSION
+    FMT = '%Y%m%dT%H%M%SZ'.freeze
+    VERSION = 'v1.23'.freeze
+    HOST = 'us-west-1.hyper.sh'.freeze
+    REGION = 'us-west-1'.freeze
+    SERVICE = 'hyper'.freeze
+    ALGORITHM = 'HYPER-HMAC-SHA256'.freeze
+    KEYPARTS_REQUEST = 'hyper_request'.freeze
+    BASE_URL = 'https://' + HOST + '/'
 
-    attr_accessor :verb, :uri, :client, :date, :headers
+    attr_accessor :verb, :uri, :client, :date, :headers, :conn
 
-    def initialize(client, path, verb = 'GET', body = '')
+    def initialize(client, uri, verb = 'GET', body = '')
       @client = client
-      @path = path
+      @uri = URI(VERSION + uri)
+      @path = @uri.path
+      @query = @uri.query
       @body = body
+      @hashed_body = hexdigest(body)
       @verb = verb.upcase
       @date = Time.now.utc.strftime(FMT)
-      @hashed_body = hash(body)
       @headers = {
-        'content-type' => 'application/json',
-        'x-hyper-date' => @date,
-        'host' => HOST,
-        'x-hyper-content-sha256' => @hashed_body
+        :content_type => 'application/json',
+        :x_hyper_date => @date,
+        :host => HOST,
+        :x_hyper_content_sha256 => @hashed_body
       }
     end
 
     def perform
       sign
-      puts 'making request . . .'
+      response = HTTP.headers(@headers).public_send(:get, BASE_URL + @uri.to_s)
+      puts response
+      response.body
     end
 
+    # join all headers sent by `;`
+    # ie:
+    # content-type;x-hyper-hmac-sha256
     def signed_headers
-      @headers.keys.sort.join(";")
+      @headers.keys.sort.map { |header| header.to_s.gsub('_', '-') }.join(';')
     end
 
     # sorts all headers, join them by `:`, and re-join by \n
     # ie:
     # content-type:application\nhost:us-west-1.hyper.sh
     def canonical_headers
-      @headers.sort.map { |header| header.join(':') }.join("\n")
+      @headers.sort.map { |header, value| "#{header.to_s.gsub('_', '-')}:#{value}" }.join("\n") + "\n"
     end
 
+    # creates Authoriatization header
     def sign
       credential = "#{@client.access_key}/#{credential_scope}"
-      auth = "#{ALGORITHM} Credential=#{credential},SignedHeaders=#{signed_headers},Signature=#{signature}"
-      puts credential
-      puts auth
+      auth = "#{ALGORITHM} Credential=#{credential}, SignedHeaders=#{signed_headers}, Signature=#{signature}"
+      @headers[:authorization] = auth
     end
 
+    # setups signature key
+    # https://docs.hyper.sh/Reference/API/2016-04-04%20[Ver.%201.23]/index.html
     def signature
-      hmac(signing_key, string_to_sign)
+      k_date = hmac('HYPER' + @client.secret_key, @date[0,8])
+      k_region = hmac(k_date, REGION)
+      k_service = hmac(k_region, SERVICE)
+      k_credentials = hmac(k_service, 'hyper_request')
+      hexhmac(k_credentials, string_to_sign)
     end
 
     def string_to_sign
@@ -63,15 +79,8 @@ module Hyperb
         ALGORITHM,
         @date,
         credential_scope,
-        hash(canonical_request())
+        hexdigest(canonical_request)
       ].join("\n")
-    end
-
-    def signing_key
-      k_date = hmac('HYPER' + @client.secret_key, @date)
-      k_region = hmac(k_date, REGION)
-      k_service = hmac(k_region, SERVICE)
-      hmac(k_service, KEYPARTS_REQUEST)
     end
 
     def canonical_request
@@ -87,19 +96,23 @@ module Hyperb
 
     def credential_scope
       [
-        @date,
+        @date[0,8],
         REGION,
         SERVICE,
         KEYPARTS_REQUEST
-      ].join('/')
+      ].join("/")
     end
 
-    def hmac(key, data)
-      OpenSSL::HMAC.hexdigest(hash(''), key, data)
+    def hexdigest(value)
+      Digest::SHA256.new.update(value).hexdigest
     end
 
-    def hash(data)
-      OpenSSL::Digest.new('sha256', data)
+    def hmac(key, value)
+      OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), key, value)
+    end
+
+    def hexhmac(key, value)
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'), key, value)
     end
 
   end
--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -2,8 +2,15 @@ require "helper"
 
 RSpec.describe Hyperb::Request do
 
-  it "date should be in right format" do
-    client = Hyperb::Client.new secret_key: 'SK'
-    client.date
+  it "#singed_headers" do
+    req = Hyperb::Request.new(Hyperb::Client.new(access_key: 'ak', secret_key: 'sk'), '/', 'get')
+    expect(req.signed_headers).to eql('content-type;host;x-hyper-content-sha256;x-hyper-date')
   end
+
+  it "#sign" do
+    req = Hyperb::Request.new(Hyperb::Client.new(access_key: 'ak', secret_key: 'sk'), '/', 'get')
+    req.sign
+    expect(req.headers[:authorization]).not_to be_nil
+  end
+
 end
