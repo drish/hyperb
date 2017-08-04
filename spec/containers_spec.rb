504ad513cff835b0ec3d6855a219164c51853d97
drish <d.erich@hotmail.com>
2017-08-04 18:33:25 -0400

    couple more tests for containers

--- spec/containers_spec.rb
+++ spec/containers_spec.rb
@@ -26,6 +26,27 @@ RSpec.describe Hyperb::Containers do
       @client.containers(all: true)
       expect(a_request(:get, @containers_path + '?all=true')).to have_been_made
     end
+
+    it 'return array of containers' do
+      stub_request(:get, @containers_path + '?all=true')
+      .to_return(body: fixture('containers.json'))
+
+      containers = @client.containers(all: true)
+      expect(containers).to be_a Array
+      expect(containers[0]).to be_a Hyperb::Container
+    end
+
+    it 'correct attrs' do
+      stub_request(:get, @containers_path)
+      .to_return(body: fixture('containers.json'))
+
+      @client.containers.each do |container|
+        expect(container.id).to be_a String
+        expect(container.created).to be_a Fixnum
+        expect(container.command).to be_a String
+      end
+    end
+
   end
 
 end
