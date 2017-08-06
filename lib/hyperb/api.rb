7d3d693793f2f8dabcbbca77e80560bcfc394752
drish <d.erich@hotmail.com>
2017-08-06 15:56:33 -0400

    [volumes]: init volumes wrapper

--- lib/hyperb/api.rb
+++ lib/hyperb/api.rb
@@ -1,5 +1,6 @@
 require 'hyperb/images/images'
 require 'hyperb/containers/containers'
+require 'hyperb/volumes/volumes'
 require 'hyperb/hyper_version'
 
 # wrapper for modules
@@ -8,6 +9,7 @@ module Hyperb
   module API
     include Hyperb::Images
     include Hyperb::Containers
+    include Hyperb::Volumes
     include Hyperb::HyperVersion
     # include Hyperb::Fips
   end
---
+++ lib/hyperb/volumes/volumes.rb
@@ -0,0 +1,28 @@
+require 'hyperb/request'
+require 'hyperb/utils'
+require 'hyperb/auth_object'
+require 'json'
+require 'uri'
+require 'base64'
+
+module Hyperb
+  module Volumes
+
+    include Hyperb::Utils
+
+    # remove volume
+    #
+    # @see https://docs.hyper.sh/Reference/API/2016-04-04%20[Ver.%201.23]/Volume/remove.html
+    #
+    # @raise [Hyperb::Error::Unauthorized] raised when credentials are not valid.
+    #
+    # @param params [Hash] A customizable set of params.
+    # @option params [String] :all default is true
+    # @option params [String] :filter only return image with the specified name
+    def remove_volume(params = {})
+      raise ArgumentError.new('Invalid arguments.') if !check_arguments(params, 'id')
+      path = '/volumes/' + params[:id]
+      Hyperb::Request.new(self, path, {}, 'delete').perform
+    end
+  end
+end
