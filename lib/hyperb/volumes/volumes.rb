1fdaa8d78c288de09ea78aa5a81db7eead1e5026
drish <d.erich@hotmail.com>
2017-08-07 16:39:09 -0400

    [volumes]: add volumes listing fn

--- lib/hyperb/volumes/volumes.rb
+++ lib/hyperb/volumes/volumes.rb
@@ -1,6 +1,6 @@
 require 'hyperb/request'
 require 'hyperb/utils'
-require 'hyperb/auth_object'
+require 'hyperb/volumes/volume'
 require 'json'
 require 'uri'
 require 'base64'
@@ -9,6 +9,26 @@ module Hyperb
   # volumes module
   module Volumes
     include Hyperb::Utils
+
+    # list volumes
+    #
+    # @see https://docs.hyper.sh/Reference/API/2016-04-04%20[Ver.%201.23]/Volume/list.html
+    #
+    # @raise [Hyperb::Error::Unauthorized] raised when credentials are not valid.
+    #
+    # @return [Hyperb::Volume] Array of Hyperb::Volume.
+    #
+    # @param params [Hash] A customizable set of params.
+    # TODO: @option params [String] :filters JSON encoded value of the filters
+    # TODO: @option params filters [Boolean] :dangling
+    def volumes(params = {})
+      path = '/volumes'
+      response = JSON.parse(Hyperb::Request.new(self, path, {}, 'get').perform)
+
+      # hyper response atm comes inside a Volumes: [], unlike the images API
+      # which is returned in a []
+      response['Volumes'].map { |vol| Hyperb::Volume.new(vol) }
+    end
 
     # remove volume
     #
