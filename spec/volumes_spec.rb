8e576010e0f337a031952c0dc48d5d0cd9382972
drish <d.erich@hotmail.com>
2017-08-07 16:15:08 -0400

    [volumes]: add spec for inspect_volume

--- spec/volumes_spec.rb
+++ spec/volumes_spec.rb
@@ -24,4 +24,34 @@ RSpec.describe Hyperb::Images do
       expect(a_request(:delete, path)).to have_been_made
     end
   end
+
+  describe '#inspect_volume' do
+
+    it 'should raise ArgumentError when id is not provided' do
+      expect { @client.inspect_volume }.to raise_error(ArgumentError)
+    end
+
+    it 'request to the correct path should be made' do
+      path = @base_path + '/id'
+
+      stub_request(:get, path)
+      .to_return(body: fixture('inspect_container.json'))
+
+      @client.inspect_volume id: 'id'
+      expect(a_request(:get, path)).to have_been_made
+    end
+
+
+    it 'correct attrs' do
+      path = @base_path + '/id'
+
+      stub_request(:get, path)
+      .to_return(body: fixture('inspect_container.json'))
+
+      vol = @client.inspect_volume id: 'id'
+      expect(vol.key?(:name)).to be true
+      expect(vol.key?(:driver)).to be true
+    end
+  end
+
 end
