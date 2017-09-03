e30b7c4b888bcfc76fef1f10f716395dc05aad23
Carlos Derich <d.erich@hotmail.com>
2017-09-03 11:41:58 -0400

    func: prepare attributes underscoring fields

--- lib/hyperb/funcs/func.rb
+++ lib/hyperb/funcs/func.rb
@@ -1,12 +1,13 @@
 module Hyperb
   # func object
   class Func
+    include Hyperb::Utils
     attr_accessor :name, :container_size, :timeout, :uuid,
                   :created, :config, :host_config, :networking_config
 
     def initialize(attrs = {})
       attrs.each do |k, v|
-        instance_variable_set("@#{k.downcase.to_sym}", v)
+        instance_variable_set("@#{underscore(k)}", v)
       end
     end
   end
