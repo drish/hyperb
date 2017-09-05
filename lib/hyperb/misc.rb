1dcee0a5737e5783ff9255b5bb7bcd5f0308a926
Carlos Derich <d.erich@hotmail.com>
2017-09-04 21:35:00 -0400

    add info function (#11)
    * misc: rename version with misc and add info endpoint
    
    * misc: add inline docs for info endpoint
    
    * spec: add spec for misc #info

--- lib/hyperb/misc.rb
+++ lib/hyperb/misc.rb
@@ -3,10 +3,15 @@ require 'json'
 
 module Hyperb
   # simple wrapper for the version endpoint
-  module HyperVersion
+  module Misc
     # returns current version of hyper.sh api
     def version
       JSON.parse(Hyperb::Request.new(self, '/version', {}, 'get').perform)
+    end
+
+    # returns system-wide information about user account
+    def info
+      JSON.parse(Hyperb::Request.new(self, '/info', {}, 'get').perform)
     end
   end
 end
